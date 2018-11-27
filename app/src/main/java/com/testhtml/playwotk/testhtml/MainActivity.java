33cc8f2ddee9941db5018b74df9221de11c9edd5
v9nineIX <v9nineix@gmail.com>
2018-11-27 19:15:12 +0700

    init Project

---
+++ app/src/main/java/com/testhtml/playwotk/testhtml/MainActivity.java
@@ -0,0 +1,28 @@
+package com.testhtml.playwotk.testhtml;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        try (final WebClient webClient = new WebClient()) {
+//            final HtmlPage page = webClient.getPage("http://htmlunit.sourceforge.net");
+//
+//        }catch (IOException e){
+//
+//        }
+
+
+    } //  end onCreate
+
+} //  end class
